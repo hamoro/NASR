4b11386eade64d76b5bf8f2998d475892658b42d
s7qclan <41242298+s7qclan@users.noreply.github.com>
2018-07-24 14:47:48 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,49 +1,28 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
-    console.log('I am ready!');
-});
-
-client.on('message', message => {
-    if (message.content === 'ping') {
-        message.reply('pong');
-      }
-});
-
 console.log("Welcome Again !");
  
  client.on("guildMemberRemove", member => {
   member.createDM().then(function (channel) {
-  return channel.send(` 
-**
-**
-
-:earth_asia: welcome TO S7Q CLAN :heart:  ,   https://discord.gg/4prW5HA
-
-
-7LMN NWSL 1000 ADW**
-
-
- ${member}  
-**`)
+  return channel.send(**
+سلام عليكم ي بعد عيني ممكن نخطفك عندنا الليلة :rose: :P 
+حلمنه نوصل 1000 عضو
+[ https://discord.gg/EsbgKV ]
+الدعووة خاصة لك ي قلبي ... [ ${member}  ]
+**)
 }).catch(console.error)
 
 })
  
 client.on("guildMemberAdd", member => {
   member.createDM().then(function (channel) {
-  return channel.send(` 
-**
-**
-
-:earth_asia: welcome TO S7Q CLAN DAOY 5SA:heart:  ,   https://discord.gg/4prW5HA
-
-7LMN NWSL 1000 ADW**
-
-
- ${member}  
-**`) 
+  return channel.send(**
+سلام عليكم ي بعد عيني ممكن نخطفك عندنا الليلة :rose: :P 
+حلمنه نوصل 1000 عضو
+[ https://discord.gg/EsbgKV ]
+الدعووة خاصة لك ي قلبي ... [ ${member}  ]
+**) 
 }).catch(console.error)
 })
 
