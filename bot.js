9c9153152abf4a8ecf341a50214915d35108ca9b
s7qclan <41242298+s7qclan@users.noreply.github.com>
2018-07-25 22:28:16 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ console.log("Welcome Again !");
 **
 
 السلام عليكم
-:earth_asia: طلب صغير تنورنه حياك كلان سحق:heart:  ,   https://discord.gg/zADTTQ
+:earth_asia: طلب صغير تنورنه حياك كلان سحق:heart:  ,   https://discord.gg/6QKmxKm
 
 حلمنه نوصل 1000 عضو**
 
@@ -27,7 +27,7 @@ client.on("guildMemberAdd", member => {
 **
 **
 السلام عليكم
-:earth_asia: طلب صغير تنورنه حياك كلان سحق:heart:  ,   https://discord.gg/zADTTQ
+:earth_asia: طلب صغير تنورنه حياك كلان سحق:heart:  ,   https://discord.gg/6QKmxKm
 
 حلمنه نوصل 1000 عضو**
 
