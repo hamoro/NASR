cffc5cfa6bc92255e724c38aea7882dd7e9bbf94
s7qclan <41242298+s7qclan@users.noreply.github.com>
2018-09-02 22:30:52 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,11 +5,7 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-        message.reply('pong');
-      }
-});
+
 
 
 client.on('ready', () => {
