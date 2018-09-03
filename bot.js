4a1b1d9a954408a0c4df8bc376098a0d8373893b
s7qclan <41242298+s7qclan@users.noreply.github.com>
2018-09-03 05:26:27 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,121 +18,49 @@ var user = {};
 var warn = {};
 
 
-client.on('message', message => { 
-        var prefix = "ا";                     
-if(!message.channel.guild) return;
-   if(message.content.startsWith(prefix + 'لوان')) {
-   if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
-   message.channel.sendFile(`https://i.imgur.com/T5WN89n.png`).then(msg => {
-   msg.react('❤')
-   .then(() => msg.react('💚'))
-   .then(() => msg.react('💜'))
-   .then(() => msg.react('💛'))
-   .then(() => msg.react('🖤'))
-   .then(() => msg.react('💙'))
-   .then(() => msg.react('❌'))
- 
- 
-   let redFilter = (reaction, user) => reaction.emoji.name === '❤' && user.id === message.author.id;
-   let greenFilter = (reaction, user) => reaction.emoji.name === '💚' && user.id === message.author.id;
-   let purpleFilter = (reaction, user) => reaction.emoji.name === '💜' && user.id === message.author.id;
-   let yellowFilter = (reaction, user) => reaction.emoji.name === '💛' && user.id === message.author.id;
-   let blackFilter = (reaction, user) => reaction.emoji.name === '🖤' && user.id === message.author.id;
-   let blueFilter = (reaction, user) => reaction.emoji.name === '💙' && user.id === message.author.id;
-   let nooneFilter = (reaction, user) => reaction.emoji.name === '❌' && user.id === message.author.id;
- 
-   let red = msg.createReactionCollector(redFilter, { time: 15000 });
-   let green = msg.createReactionCollector(greenFilter, { time: 15000 });
-   let purple = msg.createReactionCollector(purpleFilter, { time: 15000 });
-   let yellow = msg.createReactionCollector(yellowFilter, { time: 15000 });
-   let black = msg.createReactionCollector(blackFilter, { time: 15000 });
-   let blue = msg.createReactionCollector(blueFilter, { time: 15000 });
-   let noone = msg.createReactionCollector(nooneFilter, { time: 15000 });
- 
-   red.on("collect", r => {
-       message.member.addRole(message.guild.roles.find("name", "Red"));
-       message.member.removeRole(message.guild.roles.find("name", "Black"));
-       message.member.removeRole(message.guild.roles.find("name", "Yellow"));
-       message.member.removeRole(message.guild.roles.find("name", "Purple"));
-       message.member.removeRole(message.guild.roles.find("name", "Green"));
-       message.member.removeRole(message.guild.roles.find("name", "Blue"));
-       msg.delete();
-       message.channel.send(`**تم اعطائك اللون __الاحمر__.**`).then(m => m.delete(5000));
- 
-       })
-     
-       green.on("collect", r => {
-           message.member.addRole(message.guild.roles.find("name", "Green"));
-           message.member.removeRole(message.guild.roles.find("name", "Black"));
-           message.member.removeRole(message.guild.roles.find("name", "Yellow"));
-           message.member.removeRole(message.guild.roles.find("name", "Purple"));
-           message.member.removeRole(message.guild.roles.find("name", "Red"));
-           message.member.removeRole(message.guild.roles.find("name", "Blue"));
-           msg.delete();
-           message.channel.send(`**تم اعطائك اللون __الاخضر__.**`).then(m => m.delete(5000));
- 
-           })
-         
-           purple.on("collect", r => {
-               message.member.addRole(message.guild.roles.find("name", "Purple"));
-               message.member.removeRole(message.guild.roles.find("name", "Black"));
-               message.member.removeRole(message.guild.roles.find("name", "Yellow"));
-               message.member.removeRole(message.guild.roles.find("name", "Green"));
-               message.member.removeRole(message.guild.roles.find("name", "Red"));
-               message.member.removeRole(message.guild.roles.find("name", "Blue"));
-               msg.delete();
-               message.channel.send(`**تم اعطائك اللون __البنفسجي__.**`).then(m => m.delete(1000));
- 
-               })
-             
-               yellow.on("collect", r => {
-                   message.member.addRole(message.guild.roles.find("name", "Yellow"));
-                   message.member.removeRole(message.guild.roles.find("name", "Black"));
-                   message.member.removeRole(message.guild.roles.find("name", "Purple"));
-                   message.member.removeRole(message.guild.roles.find("name", "Green"));
-                   message.member.removeRole(message.guild.roles.find("name", "Red"));
-                   message.member.removeRole(message.guild.roles.find("name", "Blue"));
-                   msg.delete();
-                   message.channel.send(`**تم اعطائك اللون __الاصفر__.**`).then(m => m.delete(1000));
- 
-                   })
-                 
-                   black.on("collect", r => {
-                       message.member.addRole(message.guild.roles.find("name", "Black"));
-                       message.member.removeRole(message.guild.roles.find("name", "Yellow"));
-                       message.member.removeRole(message.guild.roles.find("name", "Purple"));
-                       message.member.removeRole(message.guild.roles.find("name", "Green"));
-                       message.member.removeRole(message.guild.roles.find("name", "Red"));
-                       message.member.removeRole(message.guild.roles.find("name", "Blue"));
-                       msg.delete();
-                       message.channel.send(`**تم اعطائك اللون __الاسود__.**`).then(m => m.delete(1000));
- 
-                       })
-                       noone.on("collect", r => {
-                           message.member.removeRole(message.guild.roles.find("name", "Yellow"));
-                           message.member.removeRole(message.guild.roles.find("name", "Purple"));
-                           message.member.removeRole(message.guild.roles.find("name", "Green"));
-                           message.member.removeRole(message.guild.roles.find("name", "Red"));
-                           message.member.removeRole(message.guild.roles.find("name", "Blue"));
-                           message.member.removeRole(message.guild.roles.find("name", "Black"));
-                           msg.delete();
-                           message.channel.send(`**تم ازالة جميع الالوان منك.**`).then(m => m.delete(1000));
- 
-                           })                                                          
-                           blue.on("collect", r => {
-                               message.member.addRole(message.guild.roles.find("name", "Blue"));
-                               message.member.removeRole(message.guild.roles.find("name", "Yellow"));
-                               message.member.removeRole(message.guild.roles.find("name", "Purple"));
-                               message.member.removeRole(message.guild.roles.find("name", "Green"));
-                               message.member.removeRole(message.guild.roles.find("name", "Red"));
-                               message.member.removeRole(message.guild.roles.find("name", "Black"));
-                               msg.delete();
-                               message.channel.send(`**تم اعطائك اللون __الازرق__.**`).then(m => m.delete(1000));
- 
- 
-                               })
-                               })
-                               }
-                               });
+var dat = JSON.parse("{}");
+function forEachObject(obj, func) {
+    Object.keys(obj).forEach(function (key) { func(key, obj[key]) })
+}
+client.on("ready", () => {
+    var guild;
+    while (!guild)
+        guild = client.guilds.find("name", "- S7Q SERVER")
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            dat[Inv] = Invite.uses;
+        })
+    })
+})
+client.on("guildMemberAdd", (member) => {
+    let channel = member.guild.channels.find('name', 'chat');
+    if (!channel) {
+        console.log("!channel fails");
+        return;
+    }
+    if (member.id == client.user.id) {
+        return;
+    }
+    console.log('made it till here!');
+    var guild;
+    while (!guild)
+        guild = client.guilds.find("name", "- S7Q SERVER")
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            if (dat[Inv])
+                if (dat[Inv] < Invite.uses) {
+                    console.log(3);
+                    console.log(`${member} joined over ${Invite.inviter}'s invite ${Invite.code}`)
+ channel.send(` :hearts: **تم دعوته من قبل ${Invite.inviter} :hearts: 
+:hearts: رابط الدعوه --> https://discord.gg/${Invite.code} :hearts:
+:hearts: عضو رقم --> ${member.guild.memberCount} :hearts: **`)            
+ }
+            dat[Inv] = Invite.uses;
+        })
+    })
+});
+let points = JSON.parse(fs.readFileSync('./fkk/3wasmPTS.json', 'utf8'));
 
 client.login(process.env.BOT_TOKEN);
