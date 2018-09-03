00a72e3e49fcaaf39bbf680dfe2bcbe4746d4c94
s7qclan <41242298+s7qclan@users.noreply.github.com>
2018-09-03 06:02:44 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ const fetchVideoInfo = require('youtube-info');
 
 const yt_api_key = "AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4";
 const prefix = '+';
-const discord_token = "توكين البوت";
+client.login(process.env.BOT_TOKEN);
 client.login(discord_token);
 client.on('ready', function() {
     console.log(`i am ready ${client.user.username}`);
